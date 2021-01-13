bcbd78ac94d245b4135f17d06e267b7d84d90a3f
adipati27ma <adipati.cs.samsung@gmail.com>
2021-01-13 23:28:27 +0700

    yoyo infinite animation for buttonHover

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 
 import { motion } from 'framer-motion';
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity, // infinite repeat (or Number)
+    },
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
@@ -13,13 +25,7 @@ const Home = () => {
     >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: '0px 0px 8px rgb(255,255,255)',
-            boxShadow: '0px 0px 8px rgb(255,255,255)',
-          }}
-        >
+        <motion.button variants={buttonVariants} whileHover="hover">
           Create Your Pizza
         </motion.button>
       </Link>
